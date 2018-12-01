473f9ed3f40c2305aff9b984c315e1eefd534fb5
Ryo Nakano <26003928+ryonakano@users.noreply.github.com>
2018-12-01 20:53:18 +0900

    Have an app icon (#8)

--- meson/post_install.py
+++ meson/post_install.py
@@ -4,7 +4,11 @@ from os import environ, path
 from subprocess import call
 
 schemadir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+iconcachedir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
 
 if not environ.get('DESTDIR'):
     print('Completing gsettings schemas…')
     call(['glib-compile-schemas', schemadir])
+
+    print('Rebuilding desktop icon cache…')
+    call(['gtk-update-icon-cache', iconcachedir])
